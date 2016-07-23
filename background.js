5fec3ff8e4feff6485dd6b25e05cc22bbc8e1690
hassan.chiang <hassan.chiang@gmail.com>
2016-07-23 23:14:01 +0800

    refactor

--- background.js
+++ background.js
@@ -2,25 +2,33 @@
 	Author: Hassan
 	Email : hassan.chiang@gmail.com
 */
+
+var data = [];//保持api获取到的需要清除的广告选择器列表
+
+var seData = [];//保持api获取到的需要清除的搜索结果列表
+
 function clear_search_result_ad(){
-	if (window.location.href.indexOf('baidu.com')>0) {
-		$("#content_left>div").each(function (){
-			var $this = $(this);
-			if($this.text().indexOf("商业推广") >= 0){
-				$this.remove();
-				console.log("删除了节点：" + $this.text());
+	for (var i = 0; i < seData.length; i++) {
+		var se = seData[i];
+		var domainSelectorMap = se.split("||");
+		if(domainSelectorMap.length >= 2){
+			if(window.location.href.indexOf(domainSelectorMap[0])>0)	{
+				var a = $(domainSelectorMap[1]);
+				if(a.length >0){
+					a.remove();
+					console.log("删除了节点：" + $this.text());
+				}
+				$(domainSelectorMap[1]).each(function (){
+					var $this = $(this);
+					if($this.text().indexOf("商业推广") >= 0){
+						$this.remove();
+						console.log("删除了节点：" + $this.text());
+					}
+				});
 			}
-		});
-	} else if(window.location.href.indexOf('www.so.com')>0)	{
-		var a = $("#m-spread-left");
-		if(a.length >0){
-			a.remove();
-			console.log("删除了节点：" + $this.text());
 		}
 	}
 }
-
-var data = [];//保持api获取到的需要清除的广告选择器列表
 
 function clear_float_ad(){
 	for (var i = 0; i < data.length ; i++) {
@@ -38,11 +46,17 @@ $(document).ready(function () {
 		data = result.data;
 		console.log("从API获取到选择器列表：")
 		console.log(data);
-		clear_search_result_ad();
-		clear_float_ad();
-		$(document).bind('DOMSubtreeModified', function () {
+		$.getJSON("https://raw.githubusercontent.com/HassanChiang/AD-BLOCKER/master/api/se-selector.json",function (result2){
+			seData = result.data;
+			console.log("从API获取需要清除的搜索结果列表：")
+			console.log(seData);
+			
 			clear_search_result_ad();
 			clear_float_ad();
+			$(document).bind('DOMSubtreeModified', function () {
+				clear_search_result_ad();
+				clear_float_ad();
+			});
 		});
 	});
 });
